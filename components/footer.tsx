3311b389c73767de00922de4007b4482145c4e41
Lilit <baregamyanlilit36@gmail.com>
2026-03-11 20:31:24 +0400

    .

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="FontArmDecorativeU bottom-0 py-10 text-center text-base bg-[#1E1E1E8C]   text-[#fcfbfb]">
-      <p className="mt-2">
+      <div className="mt-2">
         Պատրաստվել է
         <div className="flex flex-col text-center">
           <Link href="https://www.instagram.com/webinvite.am?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
@@ -15,7 +15,7 @@ export function Footer() {
           </Link>
         </div>
         կողմից
-      </p>
+      </div>
     </footer>
   );
 }
